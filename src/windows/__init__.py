1a8cbd665453b8e7e181be25417e90bab8ae90a0
ethansmadjaa <ethansmadja@icloud.com>
2024-12-13 23:39:11 +0100

    `:sparkles: Expand __init__ imports to include all window modules`

--- src/windows/__init__.py
+++ src/windows/__init__.py
@@ -1,3 +1,15 @@
 from .app import FlashcardApp
+from .study import StudyWindow, ClassSelectionWindow
+from .cards import AddCardsWindow, CardManagerWindow
+from .settings import SettingsWindow
+from .statistics import StatisticsWindow
 
-__all__ = ['FlashcardApp']
+__all__ = [
+    'FlashcardApp',
+    'StudyWindow',
+    'ClassSelectionWindow',
+    'AddCardsWindow',
+    'CardManagerWindow',
+    'SettingsWindow',
+    'StatisticsWindow'
+]
